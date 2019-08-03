b2686e76ff2d1d51796579b0a6a37ebf66d08f3d
yaya7211 <43450496+yaya7211@users.noreply.github.com>
2019-08-04 01:54:44 +0200

    Update process.php

--- Serveur/process.php
+++ Serveur/process.php
@@ -1,5 +1,5 @@
-<? php
-$ mon_fichier  =  ' messagefeed.txt ' ;
-$ handle  =  fopen ( $ mon_fichier , ' a ' ) ou  die ( ' Impossible d'ouvrir le fichier:   ' . $ mon_fichier );
-$ data  =  $ _GET [ ' u ' ] . " \ n " ;
-? >
+<?php
+$mon_fichier  =  'messagefeed.txt' ;
+$handle  =  fopen( $mon_fichier , 'a' );
+$data  =  $_GET [ 'd' ]." \ n " ;
+?>
